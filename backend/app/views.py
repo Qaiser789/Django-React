5c907ad0a4030713c890b4bcd5e4d8ed637a1f97
Qaiser789 <sutarqaiser@gmail.com>
2023-12-04 10:01:36 +0530

    Settings updated.

--- backend/app/views.py
+++ backend/app/views.py
@@ -12,6 +12,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
 # Create your views here.
 from rest_framework import status
 from django.contrib.auth.hashers import make_password
